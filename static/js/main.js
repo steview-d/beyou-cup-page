ec06d855a3bd56e046117e1e735155aeaf5454f7
Dave Laffan <dave.laffan@outlook.com>
2020-10-19 22:51:23 +0100

    Display answer from external links
    When an answer is linked to with its anchor, display the answer element
    immediately without having to navigate through categories and questions.

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,11 @@
+const content = $('.cup-search-content-inner');
 const par_el = $('.depth-one');
 var current_pos = 0;
 var lastScrollPosition = 0;
 
 function isDoubleClicked(element) {
     /*
-     * if already clicked return TRUE to
+     * if already clicked, return TRUE to
      * indicate this click is not allowed
      */
     if (element.data('isclicked')) return true;
@@ -51,30 +52,47 @@ $('.btn-bck').click(function () {
     par_el.css('left', current_pos + '%');
 });
 
-$('.scroll-up').click( () => {
+$('.scroll-up').click(() => {
     // remember current scroll Y position and then
     // scroll to top of page
     lastScrollPosition = window.scrollY;
     window.scrollTo(0, 0);
 });
 
-$('.scroll-reset').click( () => {
+$('.scroll-reset').click(() => {
     // scroll to last saved scrtoll Y position
     window.scrollTo(0, lastScrollPosition);
 });
 
 // science container toggle
-$('.science-title').click(function() {
+$('.science-title').click(function () {
     $(this).next().slideToggle();
     $(this).find('.icon-arrow').toggleClass('rotate-180');
-})
+});
 
 // Navigate to new question from a 'related question' link
-$('.related-question').click(function() {
+$('.related-question').click(function () {
     let targetId = this.dataset.dest;
-    let content = $('.cup-search-content-inner')
     let answerContainer = $(this).closest('.depth-three');
     $(answerContainer).toggleClass('v-hide');
     content.find('#' + targetId).toggleClass('v-hide');
     window.scrollTo(0, 0);
-})
+});
+
+
+const answerId = '#' + window.location.hash.substr(1);
+const directLink = answerId.includes('q');
+
+// if the url contains an anchor to a question that exists
+if (directLink && $(answerId).length) {
+    answerElement = content.find(answerId);
+    // unhide the answer element and its parent category container
+    answerElement.closest('.depth-two').toggleClass('v-hide');
+    answerElement.toggleClass('v-hide');
+    // shift the elements over to bring into view
+    par_el.css('left', '-200%');
+}
+
+// Build links for share icons - build dynamically when clicked
+// rather than within html or a script that runs on page load,
+// as there will be almost 800 links....!
